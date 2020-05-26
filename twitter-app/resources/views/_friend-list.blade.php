20d34f7ef5978aa9eb948b6f4039e6023907501c
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-05-26 21:42:32 +0700

    ADD | Setup Base Layout For The Application

---
+++ twitter-app/resources/views/_friend-list.blade.php
@@ -0,0 +1,12 @@
+<h1 class="font-bold text-xl mb-4">Friends</h1>
+
+<ul>
+  @foreach (range(1,8) as $index)
+    <li class="mb-4">
+      <div class="flex items-center text-sm">
+        <img src="https://i.pravatar.cc/48" class="rounded-full mr-2" alt="">
+        Kurniadi A. Wijaya
+      </div>
+    </li>
+  @endforeach
+</ul>
---
+++ twitter-app/resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,10 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-4">
+  <form class="" action="index.html" method="post">
+      <textarea class="w-full" placeholder="What's Up Boi ?"></textarea>
+      <hr class="my-4">
+      <footer class="flex justify-between">
+        <img src="https://i.pravatar.cc/48" class="rounded-full mr-2" alt="">
+        <button class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white" type="submit" name="button">Post A Tweet!</button>
+      </footer>
+  </form>
+</div>
